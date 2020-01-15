fb112d65c9d5315ac316843e21760ddb8385013c
mairs <mair.swartz@live.com>
2020-01-15 21:03:42 +1100

    added underscore template

--- esp-idf-template/main/main.c
+++ esp-idf-template/main/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+<%headers.map(header=>{%>
+<%=header%><%})%>
+
+<% globals.map(line=>{%>
+<%=line%><%})%>
+
+<% functions.map(line=>{%>
+<%=line%><%})%>
 
 void app_main(void)
 {
   printf("Hello world!\n");
+  <% tasks.map(line=>{%>
+  <%=line%><%})%>
 }
